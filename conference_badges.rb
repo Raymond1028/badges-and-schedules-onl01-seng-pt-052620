9b9ce87b4fb8203df232aec877c283c2b033aaa9
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-08 22:22:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,14 +11,16 @@ def my_array(array)
     
     #Write your code here.
     
-    
+   # test1 v/ 
     def badge_maker(name)
       return "Hello, my name is #{name}."
     end
+    
+    #test2
     def batch_badge_creator()
       names = ["Edsger", "Ada," ,"Charles", "Alan", "Grace", "Linus","Matz"] 
       names.each do |name|
-        badge_maker(name)
+        return "Hello, my name is #{name}."
       end
     end
     
