d12673e518d3a8fd45c9f31d9a8f4d88e49c35fe
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-08 21:21:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def my_array()
+  return "nothing" if my_array.empty
+  my_array.each do |item|
+    puts item 
+      end
+    end
+    def badge_maker(name)
+      "Hello, my name is #{name}."
+     end
+    Write your code here.
